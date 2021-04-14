fcd7489bc1ac1f90e54cb5a0d156e7dc6f6cdb27
nozaryo8 <nozaryo.c3.8910.7@gmail.com>
2021-04-15 05:06:42 +0900

    s3追加

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,21 +1,19 @@
 require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
-
-if Rails.env.production?
-  CarrierWave.configure do |config|
+CarrierWave.configure do |config|
+  if Rails.env.production?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-      # Amazon S3用の設定
       provider: 'AWS',
-      region: ENV['AWS_S3_REGION'],
       aws_access_key_id: ENV['AWS_IAM_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_IAM_ACCESS_KEY'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1'
     }
-    config.fog_directory     =  ENV['AWS_S3_BUCKET']
-    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+    config.fog_directory = "travelour"
+    config.cache_storage = :fog     # 本番時はS3にファイルを保存する
+  else
+    config.storage = :file          # 開発・テスト時はローカルにファイルを保存する
   end
 
-  # 日本語ファイル名の設定
-  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 end
