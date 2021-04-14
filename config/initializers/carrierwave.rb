8ac40d28bc2e60296dda132fa0cd0a725d8ed464
nozaryo8 <nozaryo.c3.8910.7@gmail.com>
2021-04-15 03:21:19 +0900

    s3追加

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,8 +12,11 @@ if Rails.env.production?
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['AWS_REGION'],
+      region: ENV['AWS_DEFAULT_REGION'],
       path_style: true
     }
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
   end
+  
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 end
