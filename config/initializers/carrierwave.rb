6bdeebf64372d4f1c387730ae17e140a1f916f8a
nozaryo8 <nozaryo.c3.8910.7@gmail.com>
2021-04-15 03:39:22 +0900

    s3追加

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,16 +4,15 @@ require 'carrierwave/storage/fog'
 
 if Rails.env.production?
   CarrierWave.configure do |config|
-
+    config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory = 'travelour'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['AWS_DEFAULT_REGION'],
-      path_style: true
+      region: ENV['AWS_DEFAULT_REGION']
     }
+    config.fog_directory = 'AWS_BUCKET'
     config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
   end
   
