a4f68702e0973e012478b300dd1926cc64315ac9
nozaryo8 <nozaryo.c3.8910.7@gmail.com>
2021-04-15 06:10:48 +0900

    s3追加

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -9,17 +9,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
   #リサイズ、画像形式を変更に必要
   include CarrierWave::RMagick
-  if Rails.env.production?
-    CarrierWave.configure do |config|
-      config.fog_credentials = {
-        provider: 'AWS',
-        aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-        aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-        region: 'ap-northeast-1'
-      }
-      config.fog_directory = 'travelour'
-    end
-  end
+  
   #上限変更
     process :resize_to_limit => [200, 200]
 
