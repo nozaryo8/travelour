65b46252a616d6e065bfefffdef9e9be694871a0
nozaryo8 <nozaryo.c3.8910.7@gmail.com>
2021-06-10 02:05:54 +0900

    いいねランキング追加

--- app/models/question.rb
+++ app/models/question.rb
@@ -11,4 +11,8 @@ class Question < ApplicationRecord
   validates :body, presence: true
   validates :tag_id ,presence: true
   is_impressionable
+
+  def self.last_week # 先週のいいねの数が多い順に取得
+    evaluation = Question.joins(:evaluations).where(evaluations: { created_at: 0.days.ago.prev_month..0.days.ago}).group(:id).order("count(*) desc").limit(10)
+  end
 end
